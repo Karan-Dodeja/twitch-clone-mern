9fff6b723e84e5cf7e37a15bec53770dd71f5b46
kdodeja62@gmail.com <kdodeja62@gmail.com>
2024-01-19 02:00:40 +0530

    Cleaning files and cofigure react-router-dom package for routing

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { App } from './App';
+
+import AuthPage from './AuthPage/AuthPage.js';
+import DashboardPage from './DashboardPage/DashboardPage.js';
+
+const router = createBrowserRouter([
+  { path: '/auth', element: <AuthPage /> },
+  { path: '/', element: <DashboardPage /> },
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App>
+      <RouterProvider router={router} />
+    </App>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
