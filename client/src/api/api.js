bef77272d66d68296dd661a6d8f0bec760c85d01
kdodeja62@gmail.com <kdodeja62@gmail.com>
2024-01-19 19:55:33 +0530

    Preparing HTTP Request

---
+++ client/src/api/api.js
@@ -0,0 +1,31 @@
+import axios from 'axios'
+
+// Connect server api
+const appClient = axios.create({
+    baseURL: 'htpp://localhost:5002/api',
+    timeout: 1000,
+})
+
+// Connect login server api
+export const login = async (data) => {
+    try {
+        return appClient.post('/auth/login', data);
+    } catch (exception) {
+        return {
+            error: true,
+            exception
+        }
+    }
+}
+
+// Connect register server api
+export const register = async (data) => {
+    try {
+        return appClient.post('/auth/register', data);
+    } catch (exception) {
+        return {
+            error: true,
+            exception
+        }
+    }
+}
