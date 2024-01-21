5175c12cbbce2ed4cacc63794d68dcb9491ebce1
unknown <kdodeja62@gmail.com>
2024-01-21 17:34:14 +0530

    Get channel details from DB

--- server/src/controllers/channels/getChannelDetails.js
+++ server/src/controllers/channels/getChannelDetails.js
@@ -1,10 +1,37 @@
+import User from '../../models/User.js'
+import Channel from "../../models/Channel.js"
+
 export const getChannelDetails = async (req, res) => {
-    return res.status(200).json({
-        id: 1,
-        title: 'Title',
-        description: 'description',
-        username: 'Martin',
-        isOnline: false,
-        streamUrl: 'hhtp'
-    });
+
+    try {
+        const { channelId } = req.params;
+
+        const channel = await Channel.findById(channelId);
+
+        if (!channel || !channel.isActive) {
+            return res.status(404).send('Channel not found!')
+        }
+
+        const user = await User.findOne({ channel: channelId }, { username: 1 }) // Find user
+
+        const streamUrl = 'http';
+
+        const isOnline = false; // If user Online
+
+        // get users data from DB
+        return res.status(200).json({
+            id: channel.id,
+            title: channel.title,
+            description: channel.description,
+            username: user.username,
+            isOnline,
+            streamUrl: streamUrl,
+        });
+
+    } catch (error) {
+        console.log(error)
+        return res.status(500).send('Something went wrong.')
+    }
+
+
 };
