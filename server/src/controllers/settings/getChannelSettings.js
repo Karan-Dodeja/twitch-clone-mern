8dd596e53b5b8321296a9d46ed5135e8609f9535
unknown <kdodeja62@gmail.com>
2024-01-21 21:15:08 +0530

    Fetch settings from DB

--- server/src/controllers/settings/getChannelSettings.js
+++ server/src/controllers/settings/getChannelSettings.js
@@ -1,8 +1,26 @@
+import User from '../../models/User.js';
+
 export const getChannelSettings = async (req, res) => {
     try {
-        const  { userId } = req.user;
-        return res.status(200).send("This route is secured.")
+        const { userId } = req.user;
+
+        // getting user settings from DB
+        const userData = await User.findById(userId, {
+            channel: 1,
+            username: 1
+        }).populate("channel")
+
+        return res.status(200).json({
+            id: channel._id,
+            username: userData.username,
+            title: userData.channel.title,
+            description: userData.channel.description,
+            avatarUrl: userData.channel.avatarUrl,
+            streamKey: userData.channel.streamKey,
+        })
+
     } catch (error) {
-        return res.status(500).send("Something went wrong.")
+        console.log(error);
+        return res.status(500).send("Something went wrong.");
     }
 }
