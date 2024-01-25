efc181434952c59fef043fe7ebdfdf40d309a722
unknown <kdodeja62@gmail.com>
2024-01-25 19:42:51 +0530

    Channel view description

--- client/src/DashboardPage/Content/ChannelView/ChannelDescription.js
+++ client/src/DashboardPage/Content/ChannelView/ChannelDescription.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
 
-export const ChannelDescription = () => {
+export const ChannelDescription = ({ title, username, description }) => {
   return (
-    <div className='channel-description-container'>
-      Channel Description
+    <div className="channel-description-container">
+      <span className="channel-description-title">{username}</span>
+      <span className="channel-description-subtitle">{title}</span>
+      <span className="channel-description-box">
+        <span className="channel-description">{description}</span>
+      </span>
     </div>
-  )
-}
+  );
+};
--- client/src/DashboardPage/Content/ChannelView/ChannelView.js
+++ client/src/DashboardPage/Content/ChannelView/ChannelView.js
@@ -2,14 +2,27 @@ import React from "react";
 import { Chat } from "./Chat";
 import { ChannelDescription } from "./ChannelDescription";
 
+const channelDetails = {
+  id: 1,
+  title: "Gaming Channel",
+  description: "Playing some RPG",
+  username: "Gamer",
+  isOnline: false,
+};
+
 export const ChannelView = () => {
   return (
     <div className="channel-container">
-      <div className="channel-video-description-container">
+      <div className="channel-video-description-section">
         <div className="channel-offline-placeholder">
           <span>Channel is offline.</span>
         </div>
-        <ChannelDescription />
+        <ChannelDescription
+          channelId={channelDetails.id}
+          title={channelDetails.title}
+          description={channelDetails.description}
+          username={channelDetails.username}
+        />
       </div>
       <Chat />
     </div>
