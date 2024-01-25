a3475cb07e546002bdcb6747fe1c3deb32dc65de
unknown <kdodeja62@gmail.com>
2024-01-25 16:51:50 +0530

    Render followed listing

--- client/src/DashboardPage/Sidebar/Sidebar.js
+++ client/src/DashboardPage/Sidebar/Sidebar.js
@@ -1,5 +1,43 @@
 import React from "react";
 
+const followedChannels = [
+  {
+    id: 1,
+    username: "Martin",
+    isOnline: false,
+  },
+  {
+    id: 2,
+    username: "Marn",
+    isOnline: false,
+  },
+  {
+    id: 3,
+    username: "Mart",
+    isOnline: true,
+  },
+];
+
 export const Sidebar = () => {
-  return <div className="sidebar-container">Sidebar</div>;
+  return (
+    <div className="sidebar-container">
+      <span className="sidebar-title">For you</span>
+      <span className="sidebar-subtitle">FOLLOWED CHANNELS</span>
+      {followedChannels.map((channel) => {
+        return (
+          <div key={channel.id} className="sidebar-list-item">
+            <span className="sidebar-list-username">{channel.username}</span>
+            <span
+              className="sidebar-list-status"
+              style={{
+                color: channel.isOnline ? "green" : "red",
+              }}
+            >
+              {channel.isOnline ? "Online" : "Offline"}
+            </span>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
