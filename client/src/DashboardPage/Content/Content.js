3d2585f632a14fe44c08413da10ad11812772f59
unknown <kdodeja62@gmail.com>
2024-01-25 16:57:56 +0530

    Nested Routing

--- client/src/DashboardPage/Content/Content.js
+++ client/src/DashboardPage/Content/Content.js
@@ -1,5 +1,12 @@
 import React from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 export const Content = () => {
-  return <div>Content</div>;
+  return <div className="content-container">
+  <Routes >
+    <Route path="settings" element={<div>Settings</div>} />
+    <Route path="channels" element={<div>Channels</div>} />
+    <Route path="channel/:id" element={<div>Single channel</div>} />
+  </Routes>
+  </div>;
 };
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import './index.css';
 import { App } from './App';
 
