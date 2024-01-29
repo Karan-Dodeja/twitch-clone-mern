d45ed24095844f5e8f9f2cab99ce33552d38ff3f
unknown <kdodeja62@gmail.com>
2024-01-29 15:31:17 +0530

    Auth Inpppppput Refactor

--- client/src/AuthPage/Login.js
+++ client/src/AuthPage/Login.js
@@ -1,108 +1,111 @@
-import React, { useState } from 'react'
-import { Logo } from './Logo'
-import { AuthInput } from './AuthInput'
-import { emailValidationMessage, passwordValidationMessage, validateEmail, validatePassword } from '../shared/validators'
-import { useLogin } from '../shared/hooks'
-
+import React, { useState } from "react";
+import { Logo } from "./Logo";
+import { Input } from "../shared/components";
+import {
+  emailValidationMessage,
+  passwordValidationMessage,
+  validateEmail,
+  validatePassword,
+} from "../shared/validators";
+import { useLogin } from "../shared/hooks";
 
 export const Login = ({ switchAuthhandler }) => {
-
   const { login, isLoading } = useLogin();
 
   // Set default form state
   const [formState, setFormState] = useState({
     email: {
-      value: '',
+      value: "",
       isValid: false,
-      showError: false
+      showError: false,
     },
     password: {
-      value: '',
+      value: "",
       isValid: false,
-      showError: false
+      showError: false,
     },
-  })
+  });
 
   // Handle inputs from user
   const handleInputValueChange = (value, field) => {
-    setFormState(prevState => ({
+    setFormState((prevState) => ({
       ...prevState,
       [field]: {
         ...prevState[field],
-        value: value
+        value: value,
       },
-    }))
-  }
+    }));
+  };
 
   // validating the field's
   const handleInputValidationOnBlur = (value, field) => {
     let isValid = false;
 
     switch (field) {
-      case 'email':
+      case "email":
         isValid = validateEmail(value);
         break;
-      case 'password':
+      case "password":
         isValid = validatePassword(value);
         break;
       default:
         break;
     }
 
-    setFormState(prevState => ({
+    setFormState((prevState) => ({
       ...prevState,
       [field]: {
         ...prevState[field],
         isValid,
         showError: !isValid,
       },
-    }))
-
-  }
+    }));
+  };
 
   // handle login hook pass user value through URL on button click
   const handleLogin = (event) => {
     event.preventDefault();
-    login(formState.email.value, formState.password.value)
-  }
+    login(formState.email.value, formState.password.value);
+  };
 
-  const isSubmitButtonDisable = isLoading || !formState.password.isValid || !formState.email.isValid
+  const isSubmitButtonDisable =
+    isLoading || !formState.password.isValid || !formState.email.isValid;
 
   return (
-    <div className="login-container" >
+    <div className="login-container">
       <Logo text={"Login to clone"} />
 
-      <form className='auth-form'>
-
-        <AuthInput
-          field='email'
-          label='Email'
+      <form className="auth-form">
+        <Input
+          field="email"
+          label="Email"
           value={formState.email.value}
           onChangeHandler={handleInputValueChange}
-          type='text'
+          type="text"
           onBlurHandler={handleInputValidationOnBlur}
           showErrorMessage={formState.email.showError}
           validationMessage={emailValidationMessage}
         />
 
-        <AuthInput
-          field='password'
-          label='Password'
+        <Input
+          field="password"
+          label="Password"
           value={formState.password.value}
           onChangeHandler={handleInputValueChange}
-          type='password'
+          type="password"
           onBlurHandler={handleInputValidationOnBlur}
           showErrorMessage={formState.password.showError}
           validationMessage={passwordValidationMessage}
         />
 
-        <button onClick={handleLogin} disabled={isSubmitButtonDisable}>Log in</button>
-
+        <button onClick={handleLogin} disabled={isSubmitButtonDisable}>
+          Log in
+        </button>
       </form>
 
-      <span onClick={switchAuthhandler} className='auth-form-switch-label'>Don't have an account ? Sign up</span>
-
+      <span onClick={switchAuthhandler} className="auth-form-switch-label">
+        Don't have an account ? Sign up
+      </span>
     </div>
-  )
-
-}
+  );
+};
