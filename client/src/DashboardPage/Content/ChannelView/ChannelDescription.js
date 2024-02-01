9a3eb0ce5f5375b75d982a8ca20c82f56e8550b2
unknown <kdodeja62@gmail.com>
2024-02-01 15:40:18 +0530

    Displaying follow button

--- client/src/DashboardPage/Content/ChannelView/ChannelDescription.js
+++ client/src/DashboardPage/Content/ChannelView/ChannelDescription.js
@@ -1,9 +1,30 @@
 import React from "react";
+import { useUserDetails } from "../../../shared/hooks";
 
-export const ChannelDescription = ({ title, username, description }) => {
+const FollowedButton = ({ channelId }) => {
+  return <button className="channel-follow-button">Follow</button>;
+};
+
+export const ChannelDescription = ({
+  title,
+  username,
+  channelId,
+  description,
+}) => {
+  const { isLogged } = useUserDetails();
   return (
     <div className="channel-description-container">
-      <span className="channel-description-title">{username}</span>
+      <span className="channel-description-title">
+        {username}
+        <span>
+          {isLogged && (
+            <FollowedButton
+              className="channel-follow-button"
+              channelId={channelId}
+            />
+          )}
+        </span>
+      </span>
       <span className="channel-description-subtitle">{title}</span>
       <span className="channel-description-box">
         <span className="channel-description">{description}</span>
