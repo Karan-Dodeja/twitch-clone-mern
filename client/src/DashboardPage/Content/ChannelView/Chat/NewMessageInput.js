4e182e70be55fe728cc289b246b8bd1b1c9c71bd
unknown <kdodeja62@gmail.com>
2024-02-02 18:16:13 +0530

    Message Input

--- client/src/DashboardPage/Content/ChannelView/Chat/NewMessageInput.js
+++ client/src/DashboardPage/Content/ChannelView/Chat/NewMessageInput.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useState } from "react";
 
 export const NewMessageInput = () => {
+  const [messageContent, setMessageContent] = useState("");
+
+  const handleValueChange = (e) => {
+    setMessageContent(e.target.value);
+  };
+
+  const handleSendMessage = () => {
+    setMessageContent("");
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      handleSendMessage();
+    }
+  };
+
   return (
-    <div>
-      New Message
+    <div className="chat-message-input-container">
+      <input
+        className="chat-message-input"
+        placeholder="Type message..."
+        value={messageContent}
+        onChange={handleValueChange}
+        onKeyDown={handleKeyPress}
+      />
     </div>
-  )
-}
+  );
+};
