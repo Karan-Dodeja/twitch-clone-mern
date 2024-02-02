6e5c26b200a8ef733143b6e87a7ee6d419165e59
unknown <kdodeja62@gmail.com>
2024-02-02 16:34:07 +0530

    Created chat component

--- client/src/DashboardPage/Content/ChannelView/Chat/Chat.js
+++ client/src/DashboardPage/Content/ChannelView/Chat/Chat.js
@@ -1,5 +1,15 @@
 import React from "react";
+import { Messages } from "./Messages";
+import { NewMessageInput } from "./NewMessageInput";
 
-export const Chat = () => {
-  return <div className="chat-section">Chat</div>;
+export const Chat = ({ channelId }) => {
+  return (
+    <div className="chat-section">
+      <div className="chat-title-container">
+        <span className="chat-title-text">Stream Chat</span>
+      </div>
+      <Messages messages={[]} />
+      <NewMessageInput sendMessage={() => {}} />
+    </div>
+  );
 };
---
+++ client/src/DashboardPage/Content/ChannelView/Chat/NewMessageInput.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const NewMessageInput = () => {
+  return (
+    <div>
+      New Message
+    </div>
+  )
+}
