3d7a9e51a5c97f964857dd0843e26fb3be998b9e
unknown <kdodeja62@gmail.com>
2024-02-02 16:38:38 +0530

    Chat messages container

--- client/src/DashboardPage/Content/ChannelView/Chat/Messages.js
+++ client/src/DashboardPage/Content/ChannelView/Chat/Messages.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from "react";
+
+const messagesD = [
+  {
+    author: "Jogn",
+    content: "Hellow",
+    id: 1,
+  },
+  {
+    author: "Jogn",
+    content: "Hellow",
+    id: 2,
+  },
+];
+
+const Message = ({ author, content }) => {
+  return (
+    <span className="chat-messages-message">
+      <span style={{ fontWeight: "bold" }}>{author}:</span>
+      {content}
+    </span>
+  );
+};
 
 export const Messages = () => {
   return (
-    <div>
-      Messages
+    <div className="chat-messages-container">
+      {messagesD.map((message) => (
+        <Message
+          key={message.id}
+          author={message.author}
+          content={message.content}
+        />
+      ))}
     </div>
-  )
-}
+  );
+};
