b6bb850086e1c7c3a7d81678008eb4841003aaf3
kdodeja62@gmail.com <kdodeja62@gmail.com>
2024-02-06 19:32:58 +0530

    Used Zustand to mange state

---
+++ client/src/store/store.js
@@ -0,0 +1,12 @@
+import { create } from "zustand";
+
+export const useStore = create((set) => ({
+  chatHistory: {
+    channelId: null,
+    messages: [],
+  },
+  setChatHistory: (chatHistory) =>
+    set({
+      chatHistory,
+    }),
+}));
